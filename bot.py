050896db3c6a7d39863870bc9a304183c70f1cb0
Henrik Mårtensson Etverk <henrik.martenssonetverk@gmail.com>
2022-08-03 14:23:23 +0200

    Added &inspiration feature

--- bot.py
+++ bot.py
@@ -1,18 +1,41 @@
+#Imports
 import discord
 import asyncio
+import requests
+import json
 
-
-messages = joined = 0
+#Message intents
 intents = discord.Intents.default()
 intents.members = True
-
-
-
+#Client
 client = discord.Client(intents=intents)
 
+#Main inspiration function
+def get_inspiration():
+    apiResponse = requests.get("https://zenquotes.io/api/quotes/")
+    jsonData = json.loads(apiResponse.text)
+    return jsonData[0]["q"] + "  - **" + jsonData[0]["a"] + "**"
 
-with open("token.0", "r", encoding="utf-8") as f:
+#Client events
+#Login confirmation message
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+#Checks all sent messages
+@client.event
+async def on_message(message):
+    
+    #Looks for "&inspiration" command and runs corresponding function
+    if message.content.startswith("&inspiration"):
+        inspiration = get_inspiration()
+        await message.channel.send(inspiration)
+    
+
+#Imports discord token from "token.0"
+with open("vault.0", "r", encoding="utf-8") as f:
     lines = f.readlines()
     botToken = lines[0]
 
+#Runs bot
 client.run(botToken)
